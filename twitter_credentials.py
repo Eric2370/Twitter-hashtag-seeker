190002e2356575aacaec2d4878db8c5190bc42d2
Qiren Feng <qirenf@uci.edu>
2019-06-27 13:58:04 -0700

    credentials blocked & finished comments

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,6 +1,6 @@
 #Variables that contain user credentials to access Twitter API
 
-ACCESS_TOKEN = "944420979341533184-057kyx2iY4S5vnUMTYIvBxytJDIQ1DU"
-ACCESS_TOKEN_SECRET = "ryjimp4SxbOuHPX6rQg1H9Hre1CRYEfDVBOFEbet4njpK"
-CONSUMER_KEY = "PszXUUiwZYOWDdXTAD3xOacN9"
-CONSUMER_SECRET = "JhEwwBUcYK3durSsChi1XMIXqBK82deZjQ5CNHanPvjCMsoSlo"
+ACCESS_TOKEN = "944........."
+ACCESS_TOKEN_SECRET = "ryj........."
+CONSUMER_KEY = "Psz..........."
+CONSUMER_SECRET = "JhEw........"
